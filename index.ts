51bf0b4ec9101f82a771cf139e9d2711f30060d6
Alex <yuzhao114@outlook.com>
2020-11-09 22:50:48 +1100

    Create express server & Complete KYCcheck module

--- index.ts
+++ index.ts
@@ -1,53 +1,14 @@
-var http = require("http");
-// Set request header
-const options = {
-  hostname:"australia-southeast1-reporting-290bc.cloudfunctions.net",
-  path:"/driverlicence",
-  method:"POST",
-  headers:{
-    "Content-Type":"application/json",
-    "Authorization": ' Bearer 03aa7ba718da920e0ea362c876505c6df32197940669c5b150711b03650a78cf'
-  }
-}
-// Set request body
-var rawBody={ 
-  "birthDate" : "1985-02-08", 
-  "givenName" : "James", 
-  "middleName" : "Robert", 
-  "familyName" : "Smith", 
-  "licenceNumber" : "94977000", 
-  "stateOfIssue" : "NSW", 
-  "expiryDate" : "2020-01-01" 
-}
-var body = JSON.stringify(rawBody);
+const kycCheck = require('./kycCheck.ts');
+const express = require('express');
+const app = express();
 
-// Instantiate a http request 
-// Define the function
-const request = http.request(options,async (res)=>{
-  let returnValue;
-  res.setEncoding('utf8');
-  await res.on('data', (resBody) => {
-    const resJSON = JSON.parse(resBody);
-    switch (resJSON.verificationResultCode){
-      case "Y":
-        returnValue = true;
-        break;
-      case "N":
-        returnValue = false;
-        break;
-      case "S":
-      case "D":
-        returnValue = null;
-        break;
-    }    
-  });
-  await console.log(returnValue);
+app.get('/',(req,res) => {
+    res.send("Hello");
 });
+app.get('/kycresult',kycCheck.handleKycCheck);
 
-request.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Server is up on localhost: ' + PORT);
 });
-
-// Write data to request body
-request.write(body);
-request.end();
