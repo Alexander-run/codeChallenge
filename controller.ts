2a65c6d064d9ef6ebbafaf0fecebaa512e61401f
Alex <yuzhao114@outlook.com>
2020-11-10 21:56:19 +1100

    Add data validation

--- controller.ts
+++ controller.ts
@@ -4,13 +4,45 @@
 var axios = require('axios');
 var querystring = require('querystring');
 
+// Function used to check data format
+// Date of birth (required): Format YYYY-MM-DD
+// First Name (required): Max 100 characters
+// Middle Name: Max 100 characters
+// Last Name (required): Max 100 characters
+// Licence Number (required)
+// State (required): Must be one of NSW, QLD, SA, TAS, VIC, WA, ACT, NT
+// Expiry Date: Format: YYYY-MM-DD
 function validateData(data){
-    for (var key in data){
-      if(data[key]===""){
-        return false;
-      }
-    }
-    return true;
+  let birthDateReg = /^[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+  let birthDateRegExp = new RegExp(birthDateReg);
+  let givenNameReg = /^[a-zA-Z]{1,100}$/;
+  let givenNameRegExp = new RegExp(givenNameReg);
+  let middleNameReg = /^[a-zA-Z]{1,100}$/;
+  let middleNameRegExp = new RegExp(middleNameReg);
+  let familyNameReg = /^[a-zA-Z]{1,100}$/;
+  let familyNameRegExp = new RegExp(familyNameReg);
+  let licenceNumberReg = /^\d{8}$/;
+  let licenceNumberRegExp = new RegExp(licenceNumberReg);
+  let stateList = ["NSW", "QLD", "SA", "TAS", "VIC", "WA", "ACT", "NT"]
+  let expiryDateReg = /^[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+  let expiryDateRegExp = new RegExp(expiryDateReg);
+  if(!birthDateRegExp.test(data.birthDate)){
+    return "Input BirthDate format incorrect(required YYYY-MM-DD)"
+  }else if(!givenNameRegExp.test(data.givenName)){
+    return "Input First Name is max 100 characters"
+  }else if(!middleNameRegExp.test(data.middleName)){
+    return "Input Middle Name is max 100 characters"
+  }else if(!familyNameRegExp.test(data.familyName)){
+    return "Input Family Name is max 100 characters"
+  }else if(!licenceNumberRegExp.test(data.licenceNumber)){
+    return "Input licence number must be 8 numbers"
+  }else if(stateList.indexOf(data.stateOfIssue)<0 ){
+    return "Input state must be one of NSW, QLD, SA, TAS, VIC, WA, ACT, NT"
+  }else if(!expiryDateRegExp.test(data.expiryDate)){
+    return "Input ExpiryDate format incorrect(required YYYY-MM-DD)"
+  }else{
+    return "validated";
+  }
 }
 
 module.exports = {
@@ -26,9 +58,9 @@ module.exports = {
         data = querystring.parse(reqBody);
         try {
           // Firstly validate the input data
-          if (!validateData(data)){
+          if (!(validateData(data)==="validated")){
               res.status(400).send({
-                  "message": "empty input item detected"
+                  "message": validateData(data)
               });
           }
           // Proceed KYC check by requesting third-party API
